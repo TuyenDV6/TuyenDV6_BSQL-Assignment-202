f89b65b8cd10bf038568204b0b81ed4a7bf395ea
TuyenDV6 <69579006+TuyenDV6@users.noreply.github.com>
2020-08-12 23:50:12 +0700

    Update TuyenDV6_BSQL Assignment 202.sql

--- TuyenDV6_BSQL Assignment 202.sql
+++ TuyenDV6_BSQL Assignment 202.sql
@@ -1,9 +1,9 @@
 create table TRAINEE (
              traineeId int not null identity(1,1) primary key , 
-			 fullname varchar(100) ,
-			 birthday date , 
+			 Full_Name varchar(100) ,
+			 Birth_Date date , 
 			 gender bit , 
-			 EtIq int not null , check(EtIq  between 0 and 20 ), 
+			 ET_IQ int not null , check(ET_IQ  between 0 and 20 ), 
 			 EtMath int not null , check(EtMath  between 0 and 20 ) , 
 			 EtEnglish int not null , check(EtEnglish  between 0 and 20 ), 
 			 training varchar(100) not null , 
@@ -11,13 +11,13 @@ create table TRAINEE (
 
 )
 
-insert into TRAINEE values ('Hi', '2020-1-1', 1, 16,16,19, 'net', 'good', 'aaa' )  ;
-insert into TRAINEE values ('Hi1', '2020-1-1', 1, 16,16,16, 'net', 'good','19' ) ;
-insert into TRAINEE values ('Hi2', '2020-1-1', 1, 16,16,16, 'net', 'good' ,'boy') ;
-insert into TRAINEE values ('Hi3', '2020-1-1', 1, 19,19,19, 'net', 'good' , 'girl') ;
+insert into TRAINEE values ('T', '2020-1-1', 1, 16,16,19, 'net', 'good', 'aaa' )  ;
+insert into TRAINEE values ('T1', '2020-1-1', 1, 16,16,16, 'net', 'good','19' ) ;
+insert into TRAINEE values ('T2', '2020-1-1', 1, 16,16,16, 'net', 'good' ,'boy') ;
+insert into TRAINEE values ('T3', '2020-1-1', 1, 19,19,19, 'net', 'good' , 'girl') ;
 
 
 alter table TRAINEE  add Fsolt_Account varchar(100) not null unique 
 
-select fullname,EtIq, EtMath, EtEnglish from TRAINEE group by fullname, EtIq, EtMath, EtEnglish 
-having EtIq + EtMath >=20 and EtIq >= 8 and EtMath >= 8 and EtEnglish >= 18 
+select Full_Name,ET_IQ, EtMath, EtEnglish from TRAINEE group by Full_Name, ET_IQ, EtMath, EtEnglish 
+having ET_IQ + EtMath >=20 and ET_IQ >= 8 and EtMath >= 8 and EtEnglish >= 18 
